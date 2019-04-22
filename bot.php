e2a35ca4344aeb806ad4f9b05076a5f26abb74fd
anonnato555 <47442175+anonnato555@users.noreply.github.com>
2019-04-22 15:32:02 +0700

    Add files via upload

--- bot.php
+++ bot.php
@@ -347,6 +347,14 @@ $t = var_export( $client->parseEvents() , true);
 file_put_contents("test.txt", $t );
 
 
+foreach ($events['events'] as $event) {
+	// Reply only when message sent is in 'text' format
+	if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+		// Get text sent
+		$text = $event['message']['text'];
+		$uid = $event['source']['userId'];
+		$gid = $event['source']['groupId'];
+		$timestamp = $event['timestamp'];
 
 function replyMsg($event, $client)
 {
@@ -381,55 +389,7 @@ function replyMsg($event, $client)
 			$client->replyMessage1($event['replyToken'],$a);
 
 		}
-		if(preg_match('(สวัสดี|หวัดดี|ดีค่ะ|ดีคับ|ดีครับ|ดีคร่า|ดีค่า)', $text) === 1) {
-				
-				$a1=array("สวัสดี","หวัดดี","ดีค่ะ","ดีคับ","ดีครับ","ดีคร่า","ดีค่า");
-				foreach ($a1 as $val) {
-					$chk = explode($val,$text);	
-						if ($chk[0]==""){
-							
-							$gid = $event['source']['groupId'];
-							$uid = $event['source']['userId'];
-							//$url = 'https://api.line.me/v2/bot/group/'.$gid.'/member/'.$uid; //กลุ่ม
-							$url = 'https://api.line.me/v2/bot/profile/'.$uid;			//user
-							$channelAccessToken2 = 'Z+82Dj/iMhmE3mjr2EKu+0+W5a4O0ZiLT8SiohLjwTwSINQ+Kd/v+FdHPH9vSHriwk3IkO7Kio8GWTum007bD3r8/1BCtayNWvf+cDL8FznI3YyKcJ0OazxuBuzrlvXkpn8mYfi5MwddhMfPi3JvvgdB04t89/1O/w1cDnyilFU=';
-							$header = array(
-								"Content-Type: application/json",
-								'Authorization: Bearer '.$channelAccessToken2,
-							);
-							$ch = curl_init();
-							//curl_setopt($ch, CURLOPT_HTTP_VERSION, 'CURL_HTTP_VERSION_1_1');
-							//curl_setopt($ch, CURLOPT_VERBOSE, 1);
-							//curl_setopt($curl, CURLOPT_USERAGENT, 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.0.3705; .NET CLR 1.1.4322)');
-							curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-							curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-							//curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-							curl_setopt($ch, CURLOPT_FAILONERROR, 0);		;
-							//curl_setopt($ch, CURLOPT_HTTPGET, 1);
-							//curl_setopt($ch, CURLOPT_USERAGENT, $agent);
-							//curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 0);
-							curl_setopt($ch, CURLOPT_HEADER, 0);
-							curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
-							curl_setopt($ch, CURLOPT_URL, $url);
-							
-							$profile =  curl_exec($ch);
-							curl_close($ch);
-							$obj = json_decode($profile);
-							$pathpic = explode("cdn.net/", $obj->pictureUrl);
-							$messages = [
-									"type" => "text",
-									"text" =>  "ดีครับ \n".$obj->displayName
-							];	 
-								
-							break;
-						}
-						else{
-								
-						}
-				} 
-				
-					
-			}
+		
 
 
     }
