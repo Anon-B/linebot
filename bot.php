0a3ddcfb203c030f594da385c60e9363f9522d98
anonnato555 <47442175+anonnato555@users.noreply.github.com>
2019-04-22 13:19:06 +0700

    Add files via upload

--- bot.php
+++ bot.php
@@ -148,7 +148,7 @@ if (!is_null($events['events'])) {
 
 			
 			else if (preg_match('(เตือน|ตาราง)', $text) === 1 && preg_match('(นัด)', $text) === 1) {
-				$detail = '';
+				$detail = 'ทดสอบ';
 				$memo_=array(
 
 					"23-04-2019"=>"มีนัด (23 เม.ย. 62) ",
