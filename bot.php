5c6167fcb2c65e6175c5944c2210cad7794bc518
anonnato555 <47442175+anonnato555@users.noreply.github.com>
2019-04-22 15:28:33 +0700

    Add files via upload

--- bot.php
+++ bot.php
@@ -1,5 +1,5 @@
 <?php
-
+OK
 header('Content-Type: text/html; charset=utf-8');
 
 ini_set("log_errors", 1);
@@ -368,7 +368,55 @@ function replyMsg($event, $client)
 					);
 			$client->replyMessage1($event['replyToken'],$a);
 		}
-
+		if(preg_match('(สวัสดี|หวัดดี|ดีค่ะ|ดีคับ|ดีครับ|ดีคร่า|ดีค่า)', $text) === 1) {
+				
+				$a1=array("สวัสดี","หวัดดี","ดีค่ะ","ดีคับ","ดีครับ","ดีคร่า","ดีค่า");
+				foreach ($a1 as $val) {
+					$chk = explode($val,$text);	
+						if ($chk[0]==""){
+							
+							$gid = $event['source']['groupId'];
+							$uid = $event['source']['userId'];
+							//$url = 'https://api.line.me/v2/bot/group/'.$gid.'/member/'.$uid; //กลุ่ม
+							$url = 'https://api.line.me/v2/bot/profile/'.$uid;			//user
+							$channelAccessToken2 = 'Z+82Dj/iMhmE3mjr2EKu+0+W5a4O0ZiLT8SiohLjwTwSINQ+Kd/v+FdHPH9vSHriwk3IkO7Kio8GWTum007bD3r8/1BCtayNWvf+cDL8FznI3YyKcJ0OazxuBuzrlvXkpn8mYfi5MwddhMfPi3JvvgdB04t89/1O/w1cDnyilFU=';
+							$header = array(
+								"Content-Type: application/json",
+								'Authorization: Bearer '.$channelAccessToken2,
+							);
+							$ch = curl_init();
+							//curl_setopt($ch, CURLOPT_HTTP_VERSION, 'CURL_HTTP_VERSION_1_1');
+							//curl_setopt($ch, CURLOPT_VERBOSE, 1);
+							//curl_setopt($curl, CURLOPT_USERAGENT, 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.0.3705; .NET CLR 1.1.4322)');
+							curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+							curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+							//curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+							curl_setopt($ch, CURLOPT_FAILONERROR, 0);		;
+							//curl_setopt($ch, CURLOPT_HTTPGET, 1);
+							//curl_setopt($ch, CURLOPT_USERAGENT, $agent);
+							//curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 0);
+							curl_setopt($ch, CURLOPT_HEADER, 0);
+							curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+							curl_setopt($ch, CURLOPT_URL, $url);
+							
+							$profile =  curl_exec($ch);
+							curl_close($ch);
+							$obj = json_decode($profile);
+							$pathpic = explode("cdn.net/", $obj->pictureUrl);
+							$messages = [
+									"type" => "text",
+									"text" =>  "ดีครับ \n".$obj->displayName
+							];	 
+								
+							break;
+						}
+						else{
+								
+						}
+				} 
+				
+					
+			}
 		else if (preg_match('(ด่า|เลว|ไม่ดี|โดนว่า|น่าเบื่อ|รำคาญ|ชั่ว|สันดาน|บ่น|ถูกว่า)', $msg) === 1) {
 
 			$t = 'การบ่นไม่ใช่การแก้ปัญหา และ การด่าก็ไม่ใช่วิธีการแก้ไข'; 	
