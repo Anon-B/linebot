88a0c636d0f3e25fcb6b2b330b0528bd4d46d2c2
anonnato555 <47442175+anonnato555@users.noreply.github.com>
2019-04-24 20:19:27 +0700

    Add files via upload

--- get_add.php
+++ get_add.php
@@ -3,22 +3,22 @@
 $api_key="qNge0HYBBuKUvMe59qTLBylOfo5osudi";
 $url = ('https://api.mlab.com/api/1/databases/mlab_nosql/collections/leakpoint?apiKey='.$api_key;
 
-	$new = json_encode(array(
-		'question' => 'ทำอะไรได้บ้าง',
-		'answer' => 'ทำได้ทุกอย่าง'
-	));
+$new = json_encode(array(
+	'question' => 'ทำอะไรได้บ้าง',
+	'answer' => 'ทำได้ทุกอย่าง'
+));
 
-	$opts = array(
-		'http' => array(
-			'method' => "POST",
-			'header' => "Content-type: application/json",
-			'content' => $new
-		)
-	);
+$opts = array(
+	'http' => array(
+		'method' => "POST",
+		'header' => "Content-type: application/json",
+		'content' => $new
+	)
+);
 
-	$context = stream_context_create($opts);
-	$returnVal = file_get_contents($url, false, $context);
-	echo 'Added: '.$returnVal;
-	echo '<hr>';
-	echo '<br>';
+$context = stream_context_create($opts);
+$returnVal = file_get_contents($url, false, $context);
+echo 'Added: '.$returnVal;
+echo '<hr>';
+echo '<br>';
 ?>
