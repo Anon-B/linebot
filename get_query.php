f325db1c674b415ff1d7e1e98d5557c244ac6aba
anonnato555 <47442175+anonnato555@users.noreply.github.com>
2019-04-24 16:17:09 +0700

    Add files via upload

--- get_query.php
+++ get_query.php
@@ -1,7 +1,7 @@
 <?php
 
 $api_key="qNge0HYBBuKUvMe59qTLBylOfo5osudi";
-$url_get = file_get_contents('https://api.mlab.com/api/1/databases/mlab_nosql/collections/leakpoint?apiKey='.$api_key.'&q={"_id":{"$oid":"59fc45f1bd966f6a2c713792"},"question":"สวัสดี"}');
+$url_get = ('https://api.mlab.com/api/1/databases/mlab_nosql/collections/leakpoint?apiKey='.$api_key.'&q={"_id":{"$oid":"59fc45f1bd966f6a2c713792"},"question":"สวัสดี"}');
 
 
 $json_get = file_get_contents($url_get);
@@ -9,5 +9,6 @@ $get_quest = json_decode($json_get);
 echo '<pre>';
 	print_r($get_quest);
 	exit();
-	
+
+
 ?>
