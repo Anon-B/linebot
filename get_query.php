a8cf796133f3609c62de2be99988dcb7c2fec7c5
anonnato555 <47442175+anonnato555@users.noreply.github.com>
2019-04-24 15:52:38 +0700

    Add files via upload

--- get_query.php
+++ get_query.php
@@ -1,9 +1,9 @@
-<M
+<?
 
 
 
 $api_key="qNge0HYBBuKUvMe59qTLBylOfo5osudi";
-$json_get = file_get_contents('https://api.mlab.com/api/1/databases/pwadatabase/collections/pwa_benjee?apiKey='.$api_key.'&q={"_id":{"$oid":"'59fc45f1bd966f6a2c713792'"},"'question":"สวัสดี'"});
+$url_get = file_get_contents('https://api.mlab.com/api/1/databases/pwadatabase/collections/pwa_benjee?apiKey='.$api_key.'&q={"_id":{"$oid":"'59fc45f1bd966f6a2c713792'"},"'question":"สวัสดี'"});
 
 
 $json_get = file_get_contents($url_get);
