9f4f954fe8ad71a96e3f9d6eb0a6202e9b3c8048
anonnato555 <47442175+anonnato555@users.noreply.github.com>
2019-04-24 11:06:04 +0700

    Add files via upload

--- mlab.php
+++ mlab.php
@@ -1,22 +1,13 @@
 <?
 $api_key="qNge0HYBBuKUvMe59qTLBylOfo5osudi";
 $url_get = 'https://api.mlab.com/api/1/databases/mlab_nosql/collections/leakpiont?apiKey='.$apiKey;
-	
+echo "hello";	
 
 $json_get = file_get_contents($url_get);
 $get_quest = json_decode($json_get);
 echo '<pre>';
 	print_r($get_quest);
 	exit();
-
-
-
-
-
-
-
-
-
 
 
 
