00490bacb97fd69ee7c278cd95adc12741f99ec3
anonnato555 <47442175+anonnato555@users.noreply.github.com>
2019-04-25 11:36:33 +0700

    Add files via upload

--- query_updated.php
+++ query_updated.php
@@ -4,6 +4,11 @@ $api_key="qNge0HYBBuKUvMe59qTLBylOfo5osudi";
 
 $url_up = 'https://api.mlab.com/api/1/databases/mlab_nosql/collections/leakpoint?apiKey='.$api_key.'&q={"question":"ใครครับ"}';
 
+
+
+$_id = '';
+$url_id = 'https://api.mlab.com/api/1/databases/linedb/collections/mynewcollection/'.$_id.'?apiKey='.$apiKey;
+
 $json_query = file_get_contents($url_up);
 $get_query = json_decode($json_query);
 
@@ -12,25 +17,55 @@ foreach ($get_query as &$get_query_each){
 		//get id
 	$id = $get_query_each->_id;
 	foreach ($id as $key => $value){
+	
 		if ($key === '$oid'){
 			echo $value . ": ";
+			$_id = $value;
+			
+			$newupdate = json_encode(
+			array(
+				'$set' => array('answer'=> 'ฉันไง')
+				)
+			);
+
+
+			$optsu = array(
+				'http' => array(
+					'method' => "PUT",
+					'header' => "Content-type: application/json",
+					'content' => $newupdate
+				)
+			);
+
+			$contextu = stream_context_create($optsu);
+			//echo $contextu;
+			$returnValup = file_get_contents($url_id, false, $contextu);
+			echo 'Updated: '.$returnValup; 
+			///$returnValup ถ้าอัพเดทสำเร็จ Updated: { "n" : 1}
+			echo '<hr>';
+			echo '<br>'; 
+			
+			
+			
 		}
+		
 	}
 
 		// get question and answer
 		echo $get_query_each->question . '-' . $get_query_each->answer;
 		echo '<hr>';
+		
 		//echo "lll".$value[0];
-
+	
 	}
 
 //echo 'result --' .$json_query;
 
 
-/* 
+/
 
 echo '<br>';
-$newupdate = json_encode(
+/* $newupdate = json_encode(
 	array(
 		'$set' => array('answer'=> 'นนท์เองครับ')
 		)
@@ -51,7 +86,7 @@ $returnValup = file_get_contents($url_up, false, $contextu);
 echo 'Updated: '.$returnValup; 
 ///$returnValup ถ้าอัพเดทสำเร็จ Updated: { "n" : 1}
 echo '<hr>';
-echo '<br>'; */
+echo '<br>';  */
 
 ///
 	
