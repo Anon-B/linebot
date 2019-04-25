6bf1b4a051b8fb2175d9b9b4d5ece61a88b852ed
anonnato555 <47442175+anonnato555@users.noreply.github.com>
2019-04-25 10:10:39 +0700

    Add files via upload

---
+++ query_updated.php
@@ -0,0 +1,27 @@
+<?php
+
+$api_key="qNge0HYBBuKUvMe59qTLBylOfo5osudi";
+$url_up = 'https://api.mlab.com/api/1/databases/mlab_nosql/collections/leakpoint?apiKey='.$api_key.'&q={"question":"ใครครับ"}';
+
+$newupdate = json_encode(
+	array(
+		'$set' => array('answer'=> 'นนท์เองครับ')
+		)
+	);
+$optsu = array(
+	'http' => array(
+		'method' => "PUT",
+		'header' => "Content-type: application/json",
+		'content' => $newupdate
+	)
+);
+
+$contextu = stream_context_create($optsu);
+//echo $contextu;
+$returnValup = file_get_contents($url_up, false, $contextu);
+echo 'Updated: '.$returnValup;
+echo '<hr>';
+echo '<br>';
+	
+
+?>
