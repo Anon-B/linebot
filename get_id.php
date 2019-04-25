db0f2f2da8485a55d256cfb4fb3c34cb32d11bf5
anonnato555 <47442175+anonnato555@users.noreply.github.com>
2019-04-25 09:32:59 +0700

    Add files via upload

---
+++ get_id.php
@@ -0,0 +1,25 @@
+<?php	
+$apiKey = 'qNge0HYBBuKUvMe59qTLBylOfo5osudi';
+$url_get = 'https://api.mlab.com/api/1/databases/mlab_nosql/collections/leakpiont?apiKey='.$apiKey;//
+
+
+$json_get = file_get_contents($url_get);
+$get_quest = json_decode($json_get);
+
+
+foreach ($get_quest as &$get_quest_each){
+		//get id
+	$id = $get_quest_each->_id;
+	foreach ($id as $key => $value){
+		if ($key === '$oid'){
+			echo $value . ": ";
+		}
+	}
+
+		// get question and answer
+		echo $get_quest_each->question . '-' . $get_quest_each->answer;
+		echo '<hr>';
+	}
+	exit();
+	
+?>
