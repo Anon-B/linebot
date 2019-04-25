05615ba3919dcc5e67674094f957dccc187e5b76
anonnato555 <47442175+anonnato555@users.noreply.github.com>
2019-04-25 08:58:34 +0700

    Add files via upload

--- get_update.php
+++ get_update.php
@@ -1,7 +1,7 @@
 <?php
-$api_key="qNge0HYBBuKUvMe59qTLBylOfo5osudi";
+$api_key ="qNge0HYBBuKUvMe59qTLBylOfo5osudi";
 $_id = '59fc45f1bd966f6a2c713766';
-$url_up = 'https://api.mlab.com/api/1/databases/mlab_nosql/collections/leakpoint/'.$_id.'?apiKey='.$api_Key;
+$url_up = 'https://api.mlab.com/api/1/databases/mlab_nosql/collections/leakpoint/'.$_id.'?apiKey='.$api_key;
 
 $newupdate = json_encode(
 	array(
@@ -24,6 +24,7 @@ $optsu = array(
 );
 
 $contextu = stream_context_create($optsu);
+echo $contextu;
 $returnValup = file_get_contents($url_up, false, $contextu);
 echo 'Updated: '.$returnValup;
 echo '<hr>';
